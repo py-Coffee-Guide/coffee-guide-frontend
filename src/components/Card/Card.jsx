f7c80f79d83c721d5930ef0d59271093d19bdb66
Alena Lyalyushkina <pluto_16@mail.ru>
2023-12-14 23:06:21 +1000

    feat: add card elem

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ import saveIcon from '../../assets/images/save-icon.svg';
 
 function Card() {
 	return (
-		<section className={styles.container}>
+		<section className={styles.card}>
 			<button type="button" className={styles.button_back}>
 				<img src={arrowLeft} alt="кнопка назад" />
 				<p className={styles.text}>Назад</p>
@@ -30,6 +30,32 @@ function Card() {
 					</button>
 				</div>
 			</div>
+			<div className={styles.content}>
+				<div className={styles.caption}>
+					<div className={styles.container_button}>
+						<button type="button" className={styles.button_medium}>
+							Oткрыто сейчас
+						</button>
+						<button type="button" className={styles.button_medium}>
+							Кофе с собой
+						</button>
+					</div>
+					<p className={styles.text}>ул. Улица , д. 12</p>
+					<p className={styles.text}>Сегодня с 8.00 - 21.00 </p>
+					<p className={styles.text}>Телефон +7 495 000 00 00 </p>
+					<p className={styles.text}>https://</p>
+					<p className={styles.text}>
+						У нас всегда есть свежеобжаренный кофе, который оценят любители этого напитка. В
+						ассортименте вы найдёте разные сорта и смеси со всех концов света.
+					</p>
+				</div>
+				<img className={styles.image_1} src="" alt="" />
+				<img className={styles.image_2} src="" alt="" />
+				<img className={styles.image_3} src="" alt="" />
+			</div>
+			<button className={styles.button_large} type="button">
+				Построить маршрут
+			</button>
 		</section>
 	);
 }
