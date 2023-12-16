be8195aebe40b78855afca16b80390dd44ebd9ad
Iren4ik <124435764+Iren4ik@users.noreply.github.com>
2023-12-16 20:11:54 +0300

    feat/создать модальное окно с фильтрами для поиска

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../Header/Header';
 import styles from './App.module.scss';
+import FilterModal from '../FilterModal/FilterModal.tsx';
 
 function App() {
 	const { user } = useSelector(state => state);
+
 	return (
 		<div className={styles.root}>
 			<Header />
+			<FilterModal />
 		</div>
 	);
 }
