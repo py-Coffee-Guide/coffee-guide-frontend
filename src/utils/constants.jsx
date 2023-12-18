b8c9ff58475770b2214d8d69cfdf90cae722158d
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-12-18 18:33:59 +0300

    Add main, about, tags and offers

---
+++ src/utils/constants.jsx
@@ -0,0 +1,18 @@
+export const BASE_TAGS = ['Wi-Fi', 'Открытой сейчас', 'Кофе с собой', 'Завтрак', 'Расчет по карте'];
+export const BASE_OFFERS = [
+	{
+		id: 1,
+		label: 'Интересное предложение',
+		path: 'https://plus.unsplash.com/premium_photo-1664201889896-6a42c19e953a?q=80&w=2136&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 2,
+		label: 'Статья о кофе',
+		path: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+	{
+		id: 3,
+		label: 'Интересное предложение',
+		path: 'https://images.unsplash.com/photo-1498804103079-a6351b050096?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+	},
+];
