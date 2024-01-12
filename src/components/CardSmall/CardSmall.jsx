3b900ab391f48ed9b6d828dc9756211bc7036e1b
Alena Lyalyushkina <pluto_16@mail.ru>
2024-01-12 11:03:47 +1000

    add card s

---
+++ src/components/CardSmall/CardSmall.jsx
@@ -0,0 +1,32 @@
+import styles from './CardSmall.module.scss';
+
+function CardSmall({ card, onSave, onDelete }) {
+	return (
+		<section className={styles.card}>
+			<div className={styles.photo_container}>
+				<img className={styles.photo} src={card.image} alt="фото кофейни" />
+				<button
+					type="button"
+					onClick={onSave}
+					className={styles.favourite}
+					aria-label="добавить в избранное"
+				/>
+				<button
+					type="button"
+					onClick={onDelete}
+					className={styles.delete}
+					aria-label="удалить из избранного"
+				/>
+			</div>
+
+			<div className={styles.info}>
+				<h3 className={styles.title}>{card.name}</h3>
+				<p className={styles.text}>{card.address}</p>
+				<p className={styles.text}>{card.schedule[0].text}</p>
+				<p className={styles.text}>{card.schedule[1].text}</p>
+			</div>
+		</section>
+	);
+}
+
+export default CardSmall;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,21 @@ import styles from './Main.module.scss';
 
 import SearchSection from '../SearchSection/SearchSection';
 import TagSection from '../TagSection/TagSection';
+import CardSmall from '../CardSmall/CardSmall';
+import { cards } from '../../utils/cards';
 
 function Main() {
 	return (
 		<main className={styles.main}>
 			<SearchSection />
 			<TagSection />
+			{/* <section>
+				<ul className={styles.cards_list}>
+					{cards.map(item => (
+						<CardSmall card={item} key={item.id} />
+					))}
+				</ul>
+			</section> */}
 		</main>
 	);
 }
