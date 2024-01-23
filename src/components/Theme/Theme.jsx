8cdee6cee4b8b7012cdf52c8dc0994dd57cf04fe
Alena Lyalyushkina <pluto_16@mail.ru>
2024-01-23 14:40:02 +1000

    add switcher

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import cn from 'classnames';
 
 import { set } from '../../slices/themeSlice/themeSlice';
 import styles from './Theme.module.scss';
@@ -20,21 +19,10 @@ const Theme = ({ className }) => {
 	};
 
 	return (
-		<div className="checkbox__filter">
-			<input
-				onChange={handleChange}
-				type="checkbox"
-				// checked={isChecked}
-				className="checkbox__toggler"
-			/>
-			<span className="checkbox__toggler-slider" />
-		</div>
-		// <input
-		// 	type="checkbox"
-		// 	className={cn(className, styles.root, theme === 'dark' ? styles.dark : styles.light)}
-		// 	onClick={handleChange}
-		// 	aria-label="switch"
-		// />
+		<label className={styles.switch} htmlFor="checkbox">
+			<input id="checkbox" type="checkbox" onClick={handleChange} aria-label="switch" />
+			<span className={styles.slider} />
+		</label>
 	);
 };
 
