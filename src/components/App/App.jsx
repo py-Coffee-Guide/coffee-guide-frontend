48b2a1e7e4e82a70c22ed75d5d27c34c960df709
Alena Salynskaia <al_20.07@mail.ru>
2024-01-31 22:40:07 +0300

    Prepared page for add coffeeshop

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import NewCoffeeshop from '../NewCoffeeshop/NewCoffeeshop';
 
 import styles from './App.module.scss';
 
@@ -13,6 +14,7 @@ function App() {
 			<Header />
 			<Main />
 			<Footer />
+			<NewCoffeeshop />
 		</div>
 	);
 }
