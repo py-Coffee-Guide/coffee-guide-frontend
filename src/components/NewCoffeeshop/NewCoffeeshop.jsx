845f27a847e15cbbe625810a35b239672df6ef77
Alena Salynskaia <al_20.07@mail.ru>
2024-02-04 19:15:59 +0300

    Page for add new coffeshop

--- src/components/NewCoffeeshop/NewCoffeeshop.jsx
+++ src/components/NewCoffeeshop/NewCoffeeshop.jsx
@@ -25,19 +25,21 @@ function NewCoffeeshop() {
 				</div>
 				<div className={styles.container_days}>
 					<div className={styles.days}>
-						<h3 className={styles.title}> Как в будни </h3>
 						<input className={styles.input_checkbox} type="checkbox" />
+						<h3 className={styles.title}> Как в будни </h3>
 					</div>
 					<div className={styles.days}>
-						<h3 className={styles.title}> Круглосуточно </h3>
 						<input className={styles.input_checkbox} type="checkbox" />
+						<h3 className={styles.title}> Круглосуточно </h3>
 					</div>
 				</div>
 				<div className={styles.container}>
 					<h3 className={styles.title}> Напитки</h3>
-					<input className={styles.input_type} />
-					<input className={styles.input_price} />
-					<h3 className={styles.title}> руб </h3>
+					<div>
+						<input className={styles.input_type} />
+						<input className={styles.input_price} />
+					</div>
+					<h3 className={styles.title_price}> руб </h3>
 				</div>
 				<button className={styles.add_button} type="button">
 					{' '}
@@ -47,24 +49,24 @@ function NewCoffeeshop() {
 					<h3 className={styles.title}> Доступные опции </h3>
 					<div className={styles.container_options}>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> Альтернатива </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> Безлактозное молоко </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> Сварщица </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> La Marzocco </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
-							<h3 className={styles.title_option}> Submarine </h3>
 							<input className={styles.input_checkbox} type="checkbox" />
+							<h3 className={styles.title_option}> Submarine </h3>
 						</div>
 					</div>
 				</div>
@@ -80,16 +82,16 @@ function NewCoffeeshop() {
 					<h3 className={styles.title}> Дополнительные опции </h3>
 					<div className={styles.container_options}>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> Можно с животными </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
+							<input className={styles.input_checkbox} type="checkbox" />
 							<h3 className={styles.title_option}> Продажа зерна </h3>
-							<input className={styles.input_checkbox} type="checkbox" />
 						</div>
 						<div className={styles.container_option}>
-							<h3 className={styles.title_option}> Декаф </h3>
 							<input className={styles.input_checkbox} type="checkbox" />
+							<h3 className={styles.title_option}> Декаф </h3>
 						</div>
 					</div>
 				</div>
